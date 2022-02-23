812e68334db378a8f7ca1909b28041c876ef5157
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-02-23 20:52:07 +0530

    [Vinayak] getting all values from the json file

---
+++ Employee_Rest_Sharp/Employee_Rest_Sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Employee_Rest_Sharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Employee_Rest_Sharp")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e016b88-3f7b-4477-b450-44ec555e1a9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
